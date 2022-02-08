b4527dc7573b0839096ce085b6b55ef1047304a3
hongduy67197 <destinyoblivion97@gmail.com>
2022-02-08 14:02:18 +0700

    first comment

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3050;
+
+app.get("*", (req, res) => {
+    res.send("<h1>Invalid URL</h1>");
+  });
+
+app.listen(port, ()=>{
+    console.log(`Server listern in localhost: ${port}`);
+});
