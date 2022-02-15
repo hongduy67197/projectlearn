9c4292f8d7afc7502394d6ceacd057992a2ff7c9
Dao Anh <daoanh9617@gmail.com>
2022-02-15 15:53:03 +0700

    hoàn thiện ejs

--- app.js
+++ app.js
@@ -2,21 +2,32 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3050;
-
+app.use("/pub", express.static(path.join(__dirname, "./views")));
+app.set('view engine', 'ejs');
 // sign in
 app.get('/signin', function(req, res) {
-  res.render('views/signin');
+  res.render('signin');
 });
 // sign up
 app.get('/signup', function(req, res) {
-  res.render('views/signup');
+  res.render('signup');
 });
 // categories
 app.get('/categories', function(req, res) {
-  res.render('views/categories');
+  res.render('categories');
 });
-
-
+// index
+app.get('/', function(req, res) {
+  res.render('index');
+});
+// single
+app.get('/single', function(req, res) {
+  res.render('single');
+});
+// contact
+app.get('/contact', function(req, res) {
+  res.render('contact');
+});
 //
 const Router = require("./routes");
 app.use(express.urlencoded({ extended: false }));
@@ -27,7 +38,7 @@ app.get("*", (req, res) => {
   res.send("<h1>Invalid URL</h1>");
 });
 
-app.use("/pub", express.static(path.join(__dirname, "./views")));
+
 app.use("/", Router);
 app.listen(port, () => {
   console.log(`Server listern in localhost: ${port}`);
