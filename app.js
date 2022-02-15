6c335b8072307e3197a749d9e954507ee481d197
Dao Anh <daoanh9617@gmail.com>
2022-02-15 15:03:39 +0700

    fix giao diện admin

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3050;
+// thêm ejs
+
+// sign in
+app.get('/signin', function(req, res) {
+  res.render('views/signin');
+});
+// sign up
+app.get('/signup', function(req, res) {
+  res.render('views/signup');
+});
+// categories
+app.get('/categories', function(req, res) {
+  res.render('views/categories');
+});
+
+
+//
 const Router = require("./routes");
 app.get("*", (req, res) => {
     res.send("<h1>Invalid URL</h1>");
